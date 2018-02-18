9446d09882cd396b8c924491ae04643281c77302
Jason Erdmann <jason.erdmann@tradingtechnologies.com>
2018-02-18 05:25:23 +0000

    sort of works

--- histats.go
+++ histats.go
@@ -4,12 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 )
-
-// # HELP process_virtual_memory_bytes Virtual memory size in bytes.
-// # TYPE process_virtual_memory_bytes gauge
-// process_virtual_memory_bytes 8.68343808e+08
 
 type Metric struct {
 	timestamp int64
@@ -18,49 +15,88 @@ type Metric struct {
 
 type Series struct {
 	name, help, metricType string
-	values                 []Metric
+	latest                 Metric
 }
 
 type Endpoint struct {
-	url string
+	name, url string
+}
+
+type pollResult struct {
+	metric Metric
+	ep     Endpoint
+}
+
+type metricRepo struct {
+	metrics map[string]Series
+	sync.Mutex
 }
 
 func floatMillisecond(t time.Duration) float64 {
 	return float64(t/time.Microsecond) / 1000
 }
 
-func pollEndpoint(out chan Metric, ep Endpoint, interval time.Duration) {
+func pollEndpoint(out chan pollResult, ep Endpoint, interval time.Duration) {
 	ticker := time.NewTicker(interval)
 	for range ticker.C {
 		st := time.Now()
 		_, err := http.Get(ep.url)
 		if err != nil {
-			latency := time.Now().Sub(st)
-			out <- Metric{
-				time.Now().Unix(),
-				floatMillisecond(latency)}
+			continue
 		}
+		latency := time.Now().Sub(st)
+		out <- pollResult{
+			Metric{
+				time.Now().Unix(),
+				floatMillisecond(latency)},
+			ep}
 	}
 }
 
+//func makeHandler(repo* metricRepo
+var repo metricRepo
+
 func dumpMetrics(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "rofl hi")
+	// # HELP process_virtual_memory_bytes Virtual memory size in bytes.
+	// # TYPE process_virtual_memory_bytes gauge
+	// process_virtual_memory_bytes 8.68343808e+08
+	repo.Lock()
+	defer repo.Unlock()
+	for k, v := range repo.metrics {
+		fmt.Fprintf(w, "%v_response_ms %v", k, v.latest.value)
+	}
+
 }
 
-func collectSeries(in chan Metric) {
+func collectSeries(in chan pollResult) {
 	for {
-		s := <-in
-		fmt.Printf("collect: got %s\n", s)
+		result := <-in
+		repo.Lock()
+		name := result.ep.name
+		// TODO golang doesn't allow direct assignment of map struct members?
+		// really awkward copy and reinsert for now
+		updated := repo.metrics[name]
+		updated.latest = result.metric
+		repo.metrics[name] = updated
+		repo.Unlock()
 	}
 }
 
 func main() {
-	endpoints := []string{"https://www.google.com"}
-	out := make(chan Metric)
+	// TODO no global
+	//repo := metricRepo{
+	//	metrics: make(map[string]Series),
+	//}
+	repo.metrics = make(map[string]Series)
+
+	endpoints := map[string]string{
+		"google": "https://www.google.com",
+	}
+	out := make(chan pollResult)
 
 	go collectSeries(out)
-	for _, url := range endpoints {
-		go pollEndpoint(out, Endpoint{url}, 1*time.Second)
+	for name, url := range endpoints {
+		go pollEndpoint(out, Endpoint{name, url}, 1*time.Second)
 	}
 
 	http.HandleFunc("/metrics", dumpMetrics)
