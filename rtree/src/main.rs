4d2934b80946683c9b2019feb288a4464920d5bd
Jason Erdmann <jason.erdmann@tradingtechnologies.com>
2020-07-27 22:48:13 -0500

    correct dfs, just realized node cloning is not rc cloning so ptr eq fails

--- rtree/src/main.rs
+++ rtree/src/main.rs
@@ -1,4 +1,4 @@
-use std::cell::{RefCell};
+use std::cell::RefCell;
 // use std::collections::HashSet;
 use std::collections::VecDeque;
 use std::rc::Rc;
@@ -12,21 +12,26 @@ pub struct Node {
     children: Nodes,
 }
 
+pub enum QueueLength {
+    Size(usize),
+    Null,
+}
+
 pub struct SearchResult {
-    node: Option<SharedNode>,
-    qlen: usize,
+    node: SharedNode,
+    hops: usize,
 }
 
 impl Node {
     fn new(name: &str) -> SharedNode {
-        Rc::new(RefCell::new(Node{
+        Rc::new(RefCell::new(Node {
             name: name.to_string(),
-            children: vec!(),
+            children: vec![],
         }))
     }
 
-    fn push(&mut self, node: SharedNode) {
-        self.children.push(node);
+    fn push(&mut self, node: &SharedNode) {
+        self.children.push(node.clone());
     }
 
     pub fn get_depth(&self) -> usize {
@@ -34,7 +39,7 @@ impl Node {
     }
 
     fn get_depth_impl(&self) -> usize {
-        let mut depths = vec!();
+        let mut depths = vec![];
         // grab the depth of every child recursively
         for node in &self.children {
             depths.push(node.borrow().get_depth_impl());
@@ -44,38 +49,64 @@ impl Node {
         out + 1
     }
 
-    pub fn search_breadth_first(&self, name: &str) -> SearchResult {
-        let q = RefCell::new(VecDeque::new());
-        // queue up the node rc's
-        self.search_breadth_first_impl(name, &q);
+    pub fn search_breadth_first(&self, name: &str) -> Option<SearchResult> {
+        println!("=== bfs ===");
+        let mut q: VecDeque<SharedNode> = VecDeque::new();
+        q.push_back(Rc::new(RefCell::new(self.clone())));
 
-        // walk them to see if we got a match
-        let mut idx = 0;
-        for node in q.borrow().iter() {
-            if node.name == name {
-                return SearchResult{
-                    node: Some(Rc::new(RefCell::new(node.clone()))),
-                    qlen: idx+1};
+        let mut hops = 1;
+        while !q.is_empty() {
+            let node = q.pop_front().unwrap();
+
+            println!("check {} (want {})", &node.borrow().name, &name);
+            if node.borrow().name == name {
+                println!("bfs return {:?} clone", &node.as_ptr());
+                return Some(SearchResult {
+                    node: node.clone(),
+                    hops: hops,
+                });
             }
 
-            idx += 1;
+            hops += 1;
+            for child in &node.borrow().children {
+                q.push_back(child.clone());
+            }
         }
-        return SearchResult{node: None, qlen: q.borrow().len()};
+
+        return None;
     }
 
-    fn search_breadth_first_impl(&self, name: &str, q: &RefCell<VecDeque<Node>>) {
-        // qlen += 1
-        q.borrow_mut().push_back(self.clone());
-        for node in &self.children {
-            node.borrow().search_breadth_first_impl(name, &q);
+    // todo: smash these into a common impl with two front-ends
+    // the algorithms are identical other than the queue push direction
+    pub fn search_depth_first(&self, name: &str) -> Option<SearchResult> {
+        let mut q: VecDeque<SharedNode> = VecDeque::new();
+        q.push_back(Rc::new(RefCell::new(self.clone())));
+
+        let mut hops = 1;
+        while !q.is_empty() {
+            let node = q.pop_front().unwrap();
+
+            println!("check {} (want {})", &node.borrow().name, &name);
+            if node.borrow().name == name {
+                return Some(SearchResult {
+                    node: node.clone(),
+                    hops: hops,
+                });
+            }
+
+            hops += 1;
+            for child in &node.borrow().children {
+                q.push_front(child.clone());
+            }
         }
+        return None;
     }
 
     pub fn walk(&self) {
         self.walk_impl("", true);
     }
 
-    fn walk_impl(&self, indent: &str, last: bool) {
+    fn walk_impl(&self, _indent: &str, _last: bool) {
         todo!();
         // println!("{}+- {}", indent, self.name);
 
@@ -88,11 +119,9 @@ impl Node {
 }
 
 #[cfg(test)]
-mod tests{
-    use std::cell::{RefCell};
-    use std::rc::Rc;
+mod tests {
     use super::Node;
-    use super::SearchResult;
+    use std::rc::Rc;
 
     #[test]
     fn get_depth_works() {
@@ -103,59 +132,100 @@ mod tests{
         let c1 = Node::new("c1");
         let c2 = Node::new("c2");
         let c3 = Node::new("c3");
-        top.push(c1);
-        top.push(c2);
-        top.push(c3);
+        top.push(&c1);
+        top.push(&c2);
+        top.push(&c3);
         assert_eq!(top.get_depth(), 2);
 
         let long_top = Node::new("long");
         let add_count = 999;
         //  start to the top node
         let mut cur = long_top.clone();
+
+        use rand::Rng;
+        let mut rng = rand::thread_rng();
+
         for i in 0..add_count {
-            // add a child
-            println!("{}", cur.borrow_mut().name);
-            cur.borrow_mut().push(Node::new(&format!("c{}", i)));
-            // // swing forward to the newly-added child
+            // add some children
+            for num in 0..rng.gen_range(2, 10) {
+                cur.borrow_mut()
+                    .push(&Node::new(&format!("c{}_{}", i, num)));
+            }
+            // // swing forward to the first newly-added child
             cur = cur.clone().borrow_mut().children[0].clone();
         }
-        // total is the original root plus all nodes we added
+        // total is the original root plus all levels we added
         assert_eq!(long_top.borrow().get_depth(), 1 + add_count);
     }
 
     #[test]
     fn bfs_works() {
-        let mut top = Node::new("top");
+        let top = Node::new("top");
 
         let c1 = Node::new("c1");
         let c2 = Node::new("c2");
         let c3 = Node::new("c3");
-        top.borrow_mut().push(c1);
-        top.borrow_mut().push(c2);
-        top.borrow_mut().push(c3);
+        top.borrow_mut().push(&c1);
+        top.borrow_mut().push(&c2);
+        top.borrow_mut().push(&c3);
 
-        let nil = Node::new("_nil");
-        // assert_eq!(ptr::eq(
-        //     top.search_breadth_first("top").node.as_ref().unwrap(), &top), true);
+        let d1 = Node::new("d1");
+        let d2 = Node::new("d2");
+        let d3 = Node::new("d3");
+        c1.borrow_mut().push(&d1);
+        c2.borrow_mut().push(&d2);
+        c3.borrow_mut().push(&d3);
 
-        let res = top.borrow().search_breadth_first("top");
-        assert_eq!(res.node.unwrap_or(nil.clone()).borrow().name, top.borrow().name);
-        assert_eq!(res.qlen, 1);
+        let res = top.borrow().search_breadth_first("top").unwrap();
+        println!("{:?} {:?}", &top.as_ptr(), &res.node.as_ptr());
+        assert!(Rc::ptr_eq(&top, &res.node));
+        assert_eq!(res.node.borrow().name, top.borrow().name);
+        assert_eq!(res.hops, 1);
 
-        let res = top.borrow().search_breadth_first("c3");
-        assert_eq!(res.node.unwrap_or(nil.clone()).borrow().name, top.borrow().name);
-        assert_eq!(res.qlen, 1);
+        let res = top.borrow().search_breadth_first("c2").unwrap();
+        assert_eq!(res.node.borrow().name, c2.borrow().name);
+        assert_eq!(res.hops, 3);
+
+        let res = top.borrow().search_breadth_first("d3").unwrap();
+        assert_eq!(res.node.borrow().name, d3.borrow().name);
+        assert_eq!(res.hops, 7);
 
         let res = top.borrow().search_breadth_first("not_exist");
-        assert!(res.node.unwrap_or(nil.clone()).borrow().name == nil.borrow().name);
-        assert_eq!(res.qlen, 1);
+        assert!(res.is_none());
+    }
 
-            // .unwrap_or(SearchResult{
-            // node: Some(Node::new(nil)),
-            // qlen: 0);
-            // Node::new("top").name);
-        // assert_eq!(top.search_breadth_first("nonexist").unwrap_or(&Node::new(nil)).name,
-        //     Node::new(nil).name);
+    #[test]
+    fn dfs_works() {
+        let top = Node::new("top");
+
+        let c1 = Node::new("c1");
+        let c2 = Node::new("c2");
+        let c3 = Node::new("c3");
+        top.borrow_mut().push(&c1);
+        top.borrow_mut().push(&c2);
+        top.borrow_mut().push(&c3);
+
+        let d1 = Node::new("d1");
+        let d2 = Node::new("d2");
+        let d3 = Node::new("d3");
+        c1.borrow_mut().push(&d1);
+        c2.borrow_mut().push(&d2);
+        c3.borrow_mut().push(&d3);
+
+        let res = top.borrow().search_depth_first("top").unwrap();
+        assert_eq!(res.node.borrow().name, top.borrow().name);
+        assert_eq!(res.hops, 1);
+
+        let res = top.borrow().search_depth_first("c2").unwrap();
+        assert_eq!(res.node.borrow().name, c2.borrow().name);
+        assert_eq!(res.hops, 4);
+
+        let res = top.borrow().search_depth_first("d3").unwrap();
+        assert_eq!(res.node.borrow().name, d3.borrow().name);
+        assert_eq!(res.hops, 3);
+
+        let res = top.borrow().search_depth_first("not_exist");
+        assert!(res.is_none());
     }
 }
 
