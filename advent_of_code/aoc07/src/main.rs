75833db0ae7623d75b62aaf942f8f4172f11b100
Jason Erdmann <jason.erdmann@tradingtechnologies.com>
2020-12-12 20:53:41 -0600

    part1, fix hashmap reference brainsplosions by simply copying the NodeIndex'es

--- advent_of_code/aoc07/src/main.rs
+++ advent_of_code/aoc07/src/main.rs
@@ -23,9 +23,6 @@ fn parse(buf: &str) -> Vec<Row> {
             continue;
         }
         let name = toks[0].trim();
-        // let mut cur = g.add_node(name.to_string());
-
-        // nodes.push(cur);
         if toks[1].contains("no other bags") {
             out.push(Row {
                 name: name.to_string(),
@@ -55,53 +52,56 @@ fn parse(buf: &str) -> Vec<Row> {
 type GraphT = Graph<String, String>;
 type IndexT = HashMap<String, NodeIndex>;
 
-// fn add(name: &str, g: &mut GraphT, i: &mut IndexT) -> NodeIndex {
-//     let ni = g.add_node(name.to_string);
-// }
-
-fn main() {
-    let mut buf = String::new();
-    stdin().read_to_string(&mut buf).unwrap();
-
-    let mut index: IndexT = HashMap::new();
-    let mut pending: IndexT = HashMap::new();
-
-    let rows = parse(&buf);
+fn part1(rows: &Vec<Row>, goal_name: &str) -> usize {
     let mut g = Graph::<String, String>::new();
-    // let root = g.add_node("root");
-    // let d1 = g.add_node("d1");
-    // g.extend_with_edges(&[(root, c1), (root, d1), (c1, c2)]);
+    let mut index: IndexT = HashMap::new();
 
     for row in rows {
+        // reference to the current node.  either something added during a
+        // previous iteration (eg a previous bag's child), or a new one that was
+        // newly added to the graph
+        let cur: NodeIndex;
         if let Some(exist) = index.get(&row.name) {
-            // g.extend_with_edges(&[(node, *exist)]);
+            cur = *exist;
+        } else {
+            let node = g.add_node(row.name.clone());
+            index.insert(row.name.clone(), node);
+            cur = node;
         }
 
-        let node = g.add_node(row.name.clone());
-        index.insert(row.name.clone(), node);
-        for child in row.contents {
-            if let Some(exist) = index.get(&child) {
-                g.extend_with_edges(&[(node, *exist)]);
-                println!("extend {}->{}", &row.name, &child);
-            // let ni = g.add_node(name.to_string);
-            // println!("{:?}", exist);
+        // add every child.  if the child already exists in the graph, point
+        // this new parent at it
+        for cname in &row.contents {
+            let ccur: NodeIndex;
+            if let Some(exist) = index.get(cname) {
+                ccur = *exist;
             } else {
-                let child_node = g.add_node(child.clone());
-                index.insert(child.clone(), child_node);
-                g.extend_with_edges(&[(node, child_node)]);
-                println!("new {}->{}", &row.name, &child);
+                let cnode = g.add_node(cname.clone());
+                index.insert(cname.clone(), cnode);
+                let cnode = index.get(cname).unwrap();
+                ccur = *cnode;
             }
+            g.extend_with_edges(&[(cur, ccur)]);
         }
     }
 
     let mut num = 0;
-    let goal = index.get("shiny gold").unwrap();
-    for (name, node) in &index {
-        if has_path_connecting(&g, *node, *goal, None) {
-            num += 1;
+    let goal = index.get(goal_name).unwrap();
+    for (_name, node) in &index {
+        if *node != *goal {
+            if has_path_connecting(&g, *node, *goal, None) {
+                num += 1;
+            }
         }
     }
-    println!("{}", num);
+    num
+}
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+    let rows = parse(&buf);
+    println!("{}", part1(&rows, "shiny gold"));
 }
 
 #[cfg(test)]
@@ -120,5 +120,23 @@ mod test {
         // let out = dijkstra(&g, root, None, |_| 1);
         assert!(has_path_connecting(&g, root, c2, None));
         assert!(!has_path_connecting(&g, root, z, None));
+    }
+
+    #[test]
+    fn test_part1() {
+        let mut rows = vec![];
+        rows.push(Row {
+            name: "c1".to_string(),
+            contents: vec![],
+        });
+        rows.push(Row {
+            name: "b1".to_string(),
+            contents: vec!["c1".to_string()],
+        });
+        rows.push(Row {
+            name: "d1".to_string(),
+            contents: vec!["b1".to_string()],
+        });
+        assert_eq!(part1(&rows, "c1"), 2);
     }
 }
