2d5ce0c63ee307134c589b61a5adc3512041ee11
Jason Erdmann <jason.erdmann@tradingtechnologies.com>
2021-03-06 20:36:01 -0600

    bit cleaner, still wrong

--- advent_of_code/aoc22/src/main.rs
+++ advent_of_code/aoc22/src/main.rs
@@ -5,7 +5,6 @@ type Deck = Vec<u32>;
 
 fn parse(buf: &str) -> Vec<Deck> {
     let mut decks: Vec<Deck> = vec![];
-    let mut cur: &mut Deck;
     for chunk in buf.split("Player") {
         if chunk.len() == 0 {
             continue;
@@ -69,49 +68,44 @@ fn part2(decks: &Vec<Deck>, game: usize) -> usize {
     let mut winidx;
     let mut seen: HashSet<Vec<Deck>> = HashSet::new();
     let mut round = 0;
+    println!("-- (Game {}) --", game);
     while d1.len() > 0 && d2.len() > 0 {
+        round += 1;
         // infinite loop prevention check
-        let subdecks = vec![d1[1..].to_vec(), d2[1..].to_vec()];
-        if !seen.insert(subdecks.to_vec()) {
+        // occurs before the cards are drawn each round
+        let curdecks = vec![d1.clone(), d2.clone()];
+        if !seen.insert(curdecks.to_vec()) {
+            // special case top-level game hits the infinite prevention
             if game == 1 {
                 println!("breaker part2: {:?} {:?} {}", d1, d2, score(&d1));
-                return 0;
             }
-            return 1;
+            // either way, d1 wins
+            return 0;
         }
 
-        round += 1;
-        println!("-- Round {} (Game {}) --", round, game);
-        // println!("{:?}", d1);
+        // println!("-- Round {} (Game {}) --", round, game);
+        // println!("{:?} {:?}", d1, d2);
         // println!("{:?}", d2);
-        let c1 = d1[0];
-        let c2 = d2[0];
+        let c1 = d1.remove(0);
+        let c2 = d2.remove(0);
 
-        if c1 < d1.len() as u32 && c2 < d2.len() as u32 {
+        if c1 <= d1.len() as u32 && c2 <= d2.len() as u32 {
+            let subdecks = vec![d1.clone(), d2.clone()];
             winidx = part2(&subdecks, game + 1);
             assert!(winidx < decks.len());
+        } else if c1 > c2 {
+            winidx = 0;
         } else {
-            if c1 > c2 {
-                winidx = 0;
-            } else {
-                winidx = 1;
-            }
+            winidx = 1;
         }
 
-        let win: &mut Deck;
-        let lose: &mut Deck;
         if winidx == 0 {
-            win = &mut d1;
-            lose = &mut d2;
+            d1.push(c1);
+            d1.push(c2);
         } else {
-            lose = &mut d1;
-            win = &mut d2;
+            d2.push(c2);
+            d2.push(c1);
         }
-        let mut cards = vec![];
-        cards.push(lose.remove(0));
-        cards.push(win.remove(0));
-        win.push(cards.pop().unwrap());
-        win.push(cards.pop().unwrap());
     }
 
     // original root game?
@@ -130,6 +124,7 @@ fn part2(decks: &Vec<Deck>, game: usize) -> usize {
     if d1.len() > 0 {
         return 0;
     } else {
+        assert!(d2.len() > 0);
         return 1;
     }
 }
