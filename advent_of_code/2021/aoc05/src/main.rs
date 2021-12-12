7d58d59cae0ca40ec3af64479182291e99351e14
Jason Erdmann <jerdmann83@gmail.com>
2021-12-12 09:30:57 -0600

    part2

--- advent_of_code/2021/aoc05/src/main.rs
+++ advent_of_code/2021/aoc05/src/main.rs
@@ -1,14 +1,22 @@
 use std::cmp;
 use std::io::{stdin, Read};
+use std::ops::AddAssign;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 struct Point {
-    x: usize,
-    y: usize,
+    x: i32,
+    y: i32,
+}
+
+impl AddAssign for Point {
+    fn add_assign(&mut self, other: Self) {
+        self.x += other.x;
+        self.y += other.y;
+    }
 }
 
 impl Point {
-    fn new(x: usize, y: usize) -> Self {
+    fn new(x: i32, y: i32) -> Self {
         Point { x: x, y: y }
     }
 
@@ -18,9 +26,29 @@ impl Point {
             return None;
         }
         Some(Self {
-            x: toks[0].parse::<usize>().unwrap(),
-            y: toks[1].parse::<usize>().unwrap(),
+            x: toks[0].parse::<i32>().unwrap(),
+            y: toks[1].parse::<i32>().unwrap(),
         })
+    }
+
+    fn move_towards(&mut self, dest: &Point) {
+        if *self == *dest {
+            return;
+        }
+
+        let delta;
+        if self.x == dest.x {
+            let mv = if self.y < dest.y { 1 } else { -1 };
+            delta = Point::new(0, mv);
+        } else if self.y == dest.y {
+            let mv = if self.x < dest.x { 1 } else { -1 };
+            delta = Point::new(mv, 0);
+        } else {
+            let x = if self.x < dest.x { 1 } else { -1 };
+            let y = if self.y < dest.y { 1 } else { -1 };
+            delta = Point::new(x, y);
+        }
+        *self += delta;
     }
 }
 
@@ -28,8 +56,9 @@ impl Point {
 struct Line {
     p1: Point,
     p2: Point,
-    // TODO:  these cannot possibly be right?  what if you need multiple
-    // iterators over the same struct at the same time?
+    // I'm clearly missing something with dealing with iterator state.
+    // This totally falls apart if you need multiple iterators over the
+    // same struct at the same time, right?
     cur: Point,
     has_next: bool,
 }
@@ -48,36 +77,18 @@ impl Line {
 impl Iterator for Line {
     type Item = Point;
     fn next(&mut self) -> Option<Self::Item> {
-        if !self.has_next {
-            return None;
-        }
-
-        // awful hack around my lack of expertise on the borrow checker
-        let tmp = self.cur.clone();
-
-        // move from the last position towards p2
-        let &mut pos;
-        let &dest;
-        if self.p1.x == self.p2.x {
-            pos = &mut self.cur.y;
-            dest = &self.p2.y;
-        } else {
-            pos = &mut self.cur.x;
-            dest = &self.p2.x;
-        }
-
-        let out;
-        if *pos == *dest {
-            out = None;
-        } else {
-            out = Some(tmp);
-            if *pos < *dest {
-                *pos += 1;
+        if self.cur == self.p2 {
+            if self.has_next {
+                self.has_next = false;
+                return Some(self.cur);
             } else {
-                *pos -= 1;
+                return None;
             }
         }
-        out
+
+        let tmp = self.cur.clone();
+        self.cur.move_towards(&self.p2);
+        Some(tmp)
     }
 }
 
@@ -95,23 +106,26 @@ fn parse(buf: &str) -> Vec<Line> {
     out
 }
 
-fn part1(lines: Vec<Line>) -> usize {
-    let mut max = Point::new(0, 0);
-    for line in &lines {
-        max.x = cmp::max(max.x, line.p1.x);
-        max.x = cmp::max(max.x, line.p2.x);
-        max.y = cmp::max(max.y, line.p1.y);
-        max.y = cmp::max(max.y, line.p2.y);
-    }
+#[derive(PartialEq)]
+enum Part {
+    Part1,
+    Part2,
+}
 
+fn solve(lines: Vec<Line>, part: Part) -> usize {
     let mut grid = vec![];
-    for y in 0..max.y + 1 {
-        grid.push(vec![0; max.x as usize + 1]);
+    for y in 0..1000 {
+        grid.push(vec![0; 1000]);
     }
     let mut out = 0;
     for line in lines {
-        // only horizontal/vertical lines
+        let add: bool;
         if line.p1.x == line.p2.x || line.p1.y == line.p2.y {
+            add = true;
+        } else {
+            add = part == Part::Part2;
+        }
+        if add {
             for point in line {
                 grid[point.y as usize][point.x as usize] += 1;
             }
@@ -119,7 +133,6 @@ fn part1(lines: Vec<Line>) -> usize {
     }
 
     for y in 0..grid.len() {
-        println!("{:?}", grid[y]);
         for x in 0..grid[y].len() {
             if grid[y][x] > 1 {
                 out += 1;
@@ -134,8 +147,8 @@ fn main() {
     stdin().read_to_string(&mut buf).unwrap();
     let lines = parse(&buf);
 
-    println!("part1: {}", part1(lines.clone()));
-    // println!("part2: {}", part2(game.clone()));
+    println!("part1: {}", solve(lines.clone(), Part::Part1));
+    println!("part2: {}", solve(lines.clone(), Part::Part2));
 }
 
 mod test {
@@ -154,7 +167,7 @@ mod test {
         }
 
         let mut expect = vec![];
-        for i in 0..lim+1 {
+        for i in 0..lim + 1 {
             expect.push(Point::new(i, 0));
         }
         assert_eq!(points, expect);
@@ -173,8 +186,27 @@ mod test {
         }
 
         let mut expect = vec![];
-        for i in 0..lim+1 {
+        for i in 0..lim + 1 {
             expect.push(Point::new(0, lim - i));
+        }
+        assert_eq!(points, expect);
+    }
+
+    #[test]
+    fn test_line_iterate_diagonal() {
+        let lim = 3;
+        let p1 = Point::new(0, 0);
+        let p2 = Point::new(lim, lim);
+        let mut line = Line::new(p1, p2);
+
+        let mut points = vec![];
+        while let Some(p) = line.next() {
+            points.push(p);
+        }
+
+        let mut expect = vec![];
+        for i in 0..lim + 1 {
+            expect.push(Point::new(i, i));
         }
         assert_eq!(points, expect);
     }
