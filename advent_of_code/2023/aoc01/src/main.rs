61cc3422c7e3ced6d1b2b3345975eabb5f87987e
Jason Erdmann <jerdmann83@gmail.com>
2023-12-02 08:21:17 -0600

    test passes but real input regrettably fails

--- advent_of_code/2023/aoc01/src/main.rs
+++ advent_of_code/2023/aoc01/src/main.rs
@@ -1,6 +1,6 @@
 use std::io::{stdin, Read};
 
-fn part1(buf: String) -> u32 {
+fn part1(buf: &str) -> u32 {
     let mut vals = vec![];
     for l in buf.split('\n') {
         let mut digits = vec![];
@@ -8,7 +8,7 @@ fn part1(buf: String) -> u32 {
             let val = c.to_digit(10);
             match val {
                 Some(x) => digits.push(x),
-                None => {},
+                None => {}
             }
         }
         if digits.len() < 1 {
@@ -19,23 +19,18 @@ fn part1(buf: String) -> u32 {
     return vals.into_iter().reduce(|a, b| a + b).unwrap();
 }
 
-const NUMBERS : [&'static str; 9] = [
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
+const NUMBERS: [&'static str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
 ];
 
 fn digit_from_str(s: &str) -> (Option<u8>, usize) {
     let mut val = 1;
     for n in NUMBERS {
-        if s == n {
-            return (Some(val), s.len());
+        let res = s.find(n);
+        if res.is_some() {
+            if res.unwrap() == 0 {
+                return (Some(val), n.len());
+            }
         }
         val += 1;
     }
@@ -43,24 +38,30 @@ fn digit_from_str(s: &str) -> (Option<u8>, usize) {
     (None, 0)
 }
 
-fn part2(buf: String) -> u32 {
+fn part2(buf: &str) -> u32 {
     let mut vals = vec![];
     for l in buf.split('\n') {
         let mut digits = vec![];
-        for mut i in 0..l.len() {
-            let c = l.get(i..i).unwrap();
+        let mut i = 0;
+        while i < l.len() {
+            let c = l.get(i..i+1).unwrap();
             let val = c.parse::<u32>();
             match val {
-                Result::Ok(x) => digits.push(x),
+                Result::Ok(x) => {
+                    digits.push(x);
+                    i += 1;
+                },
                 _ => {
-                    let remain : &str = l.get(i..).unwrap();
+                    let remain: &str = l.get(i..).unwrap();
                     let result = digit_from_str(remain);
                     match result {
-                        ( Some(x), len ) => {
+                        (Some(x), wordlen) => {
                             digits.push(x as u32);
-                            i += len;
-                        },
-                            _ => {},
+                            i += wordlen;
+                        }
+                        _ => {
+                            i += 1;
+                        }
                     }
                 }
             }
@@ -73,11 +74,11 @@ fn part2(buf: String) -> u32 {
     return vals.into_iter().reduce(|a, b| a + b).unwrap();
 }
 
-
 fn main() {
     let mut buf = String::new();
     stdin().read_to_string(&mut buf).unwrap();
-    println!("part1: {}", part1(buf));
+    println!("part1: {}", part1(&buf));
+    println!("part2: {}", part2(&buf));
 }
 
 #[cfg(test)]
@@ -90,8 +91,9 @@ mod test {
 1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
-treb7uchet".to_string();
-        assert!(part1(input) == 142);
+treb7uchet"
+            .to_string();
+        assert_eq!(part1(&input), 142);
     }
 
     #[test]
@@ -102,7 +104,8 @@ abcone2threexyz
 xtwone3four
 4nineeightseven2
 zoneight234
-7pqrstsixteen".to_string();
-        assert!(part2(input) == 281);
+7pqrstsixteen"
+            .to_string();
+        assert_eq!(part2(&input), 281);
     }
 }
