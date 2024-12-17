edbf2da66daca515854f7e6e09b84204f04b485e
Jason Erdmann <jerdmann83@gmail.com>
2024-12-16 21:19:17 -0600

    06 part2 passes, but brute force approach is horribly slow on the real input map.  try something smarter

--- advent_of_code/2024/aoc06/src/main.rs
+++ advent_of_code/2024/aoc06/src/main.rs
@@ -22,8 +22,12 @@ impl Point {
     }
     fn from_dir(d: Dir) -> Self {
         match d {
-            Dir::Up => Self::new(0, 1),
-            Dir::Down => Self::new(0, -1),
+            // TODO: the upside-down nature of my grid parse means both
+            // the up/down directions as well as the left-right rotate are
+            // reversed from what might be more intuitive 
+            // what's the idiomatic way to do this in 2d games programming?
+            Dir::Up => Self::new(0, -1),
+            Dir::Down => Self::new(0, 1),
             Dir::Left => Self::new(-1, 0),
             Dir::Right => Self::new(1, 0),
         }
@@ -32,15 +36,16 @@ impl Point {
         let prev = self.clone();
         match d {
             Dir::Left => {
-                *self = Self::new(-prev.y, prev.x);
+                *self = Self::new(prev.y, -prev.x);
             }
             Dir::Right => {
-                *self = Self::new(prev.y, -prev.x);
+                *self = Self::new(-prev.y, prev.x);
             },
             _ => unreachable!(),
         }
     }
 }
+
 impl ops::Add<Point> for Point {
     type Output = Point;
     fn add(self, rhs: Point) -> Point {
@@ -54,23 +59,46 @@ struct Guard {
     dir: Point,
 }
 
+type Grid = Vec<Vec<char>>;
+
 #[derive(Clone, Debug)]
 struct Map {
-    grid: Vec<Vec<char>>,
+    pub grid: Grid,
+}
+
+const EMPTY : char = '.';
+const GUARD : char = '^';
+const OBSTACLE : char = '#';
+const NEW_OBSTACLE : char = 'O';
+
+fn parse(s: &str) -> (Map, Guard) {
+    let mut grid : Grid = vec![];
+    for l in s.split('\n') {
+        grid.push(l.chars().collect());
+    }
+    // we need the first row added to be the top of the grid,
+    // or the highest y position.  the last added must be 0
+    // grid.reverse();
+
+    // now go back and find the guard
+    let mut guards = vec![];
+    for y in 0..grid.len() {
+        for x in 0..grid[y].len() {
+            if grid[y][x] == GUARD {
+                let pos = Point::new(x as i32, y as i32);
+                guards.push(Guard{pos, dir: Point::from_dir(Dir::Up)});
+                // re-normalize it as empty space
+                grid[y][x] = EMPTY;
+            }
+        }
+    }
+
+    let map = Map{ grid };
+    assert!(guards.len() == 1);
+    ( map, guards[0] )
 }
 
 impl Map {
-    fn from_str(s: &str) -> Self {
-        let mut grid = vec![];
-        for l in s.split('\n') {
-            grid.push(l.chars().collect());
-        }
-        grid.reverse();
-        // we need the first row added to be the top of the grid,
-        // or the highest y position.  the last added must be 0
-        Self{ grid }
-    }
-
     fn contains(&self, p: Point) -> bool {
         p.x > -1 && p.y > -1
                 && p.x < self.grid[0].len() as i32
@@ -94,22 +122,65 @@ impl Map {
         return Some(c);
     }
 
-    fn find(&self, c: char) -> Option<Point> {
-        for y in 0..self.grid.len() {
-            for x in 0..self.grid[y].len() {
-                if self.grid[y][x] == c {
-                    return Some(Point::new(x as i32, y as i32));
+    fn print(grid: &Grid, g: &Guard) {
+        for y in 0..grid.len() {
+            for x in 0..grid[y].len() {
+                let c;
+                if g.pos == Point::new(x as i32, y as i32) {
+                    c = '^';
+                } else {
+                    c = grid[y][x];
                 }
+                print!("{}", c);
+            }
+            print!("{}", "\n");
+        }
+    }
+
+    // not a typo.  this interface moves out of self
+    // the reason is we don't want the obstacles that were
+    // added from previous runs to leak into future ones
+    fn is_loop(mut self, mut g: Guard, obs: Point) -> bool {
+        assert!(self.contains(obs));
+        if obs == g.pos {
+            return false;
+        }
+
+        // add the new obstacle to the grid
+        self.grid[obs.y as usize][obs.x as usize] = NEW_OBSTACLE;
+
+        let mut seen : HashSet<(Point, Point)> = HashSet::new();
+        loop {
+            assert!(g.pos != obs);
+            // if we see the same tuple of guard pos/dir
+            // it means we're in a loop
+            if seen.contains(&(g.pos, g.dir)) {
+                return true;
+            }
+            seen.insert((g.pos, g.dir));
+            let np = self.peek(g.pos, g.dir);
+            // escaped the map?
+            if np.is_none() {
+                return false;
+            }
+            // otherwise, what tile are we looking at?
+            let np = np.unwrap();
+            let cur = self.get(np).unwrap();
+            match cur {
+                OBSTACLE | NEW_OBSTACLE => {
+                    g.dir.rotate(Dir::Right);
+                },
+                EMPTY => {
+                    g.pos = g.pos + g.dir;
+                },
+                _ => unreachable!(),
             }
         }
-        None
     }
 }
 
 fn part1(buf: &str) -> u32 {
-    let m = Map::from_str(buf);
-    let pos = m.find('^').unwrap();
-    let mut g = Guard{pos, dir: Point::from_dir(Dir::Up)};
+    let (m, mut g) = parse(buf);
     let mut seen : HashSet<Point> = HashSet::new();
     loop {
         seen.insert(g.pos);
@@ -122,15 +193,10 @@ fn part1(buf: &str) -> u32 {
         let np = np.unwrap();
         let cur = m.get(np).unwrap();
         match cur {
-            '#' => {
-                let last = g.dir;
+            OBSTACLE | NEW_OBSTACLE => {
                 g.dir.rotate(Dir::Right);
-
             },
-            // TODO: starting guard space is also empty
-            // normalize in map create?
-            '.' | '^' => {
-                // TODO: += operator / trait thing
+            EMPTY => {
                 g.pos = g.pos + g.dir;
             },
             _ => todo!(),
@@ -140,15 +206,29 @@ fn part1(buf: &str) -> u32 {
 }
 
 fn part2(buf: &str) -> u32 {
-    todo!();
+    let (m, g) = parse(buf);
+    let mut out = 0;
+    for y in 0..m.grid.len() {
+        for x in 0..m.grid[y].len() {
+            let obs = Point::new(x as i32, y as i32);
+            let mn = m.clone();
+            // there has to be a better way to design this that doesn't require
+            // a clone on every invocation
+            // maybe a second obstacle grid-layer that lives on top of the
+            // underlying map itself?
+            if mn.is_loop(g.clone(), obs) {
+                out += 1;
+            }
+        }
+    }
+    out
 }
-
 
 fn main() {
     let mut buf = String::new();
     stdin().read_to_string(&mut buf).unwrap();
     println!("part1: {}", part1(&buf));
-    // println!("part2: {}", part2(&buf));
+    println!("part2: {}", part2(&buf));
 }
 
 #[cfg(test)]
@@ -172,5 +252,16 @@ mod test {
 
     #[test]
     fn example2() {
+        let input = "....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...";
+        assert_eq![part2(input), 6];
     }
 }
