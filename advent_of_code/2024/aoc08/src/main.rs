5dc7db7462a866f7c46ba69431a0e45301d8130b
Jason Erdmann <jerdmann83@gmail.com>
2024-12-21 08:50:51 -0600

    test passes, real too high.  possible I have the wrong puzzle input?

--- advent_of_code/2024/aoc08/src/main.rs
+++ advent_of_code/2024/aoc08/src/main.rs
@@ -1,20 +1,70 @@
 use std::io::{stdin, Read};
 use util::Point;
+use std::collections::HashMap;
+use std::collections::HashSet;
 
 type Grid = Vec<Vec<char>>;
-
+type Antenna = HashMap<char, Vec<Point>>;
 #[derive(Clone, Debug)]
 struct Map {
     grid: Grid,
+    antenna: Antenna,
+}
+
+
+fn parse(s: &str) -> Map {
+    let mut grid = vec![];
+    let mut antenna : Antenna = Antenna::new();
+    for (yi,l) in s.split("\n").enumerate() {
+        let mut row = vec![];
+        for (xi,c) in l.chars().enumerate() {
+            if c != EMPTY {
+                let p = Point::new(xi as i32, yi as i32);
+                antenna.entry(c).or_insert(vec![]).push(p);
+            }
+            row.push(c);
+        }
+        grid.push(row);
+    }
+    Map{ grid, antenna }
 }
 
 const EMPTY : char = '.';
-const GUARD : char = '^';
-const OBSTACLE : char = '#';
-const NEW_OBSTACLE : char = 'O';
+
+impl Map {
+    fn get_antinodes(&self) -> HashSet<Point> {
+        let mut out = HashSet::new();
+        for (c, points) in &self.antenna {
+            for p1 in points {
+                for p2 in points {
+                    if *p1 == *p2 {
+                        continue;
+                    }
+                    let slope = *p2 - *p1;
+                    let dst = *p2 + slope;
+                    if self.contain(dst) {
+                        println!("{} at {:?}", c, dst);
+
+                        out.insert(dst);
+                    }
+                }
+            }
+        }
+        out
+    }
+
+    fn contain(&self, p: Point) -> bool {
+        p.x > -1 && p.y > -1
+            && p.x < self.grid.len() as i32
+            && p.y < self.grid[0].len() as i32
+    }
+}
 
 fn part1(buf: &str) -> u32 {
-    todo!();
+    let m = parse(buf);
+    println!("{:?}", m);
+    let nodes = m.get_antinodes();
+    nodes.len() as u32
 }
 
 fn part2(buf: &str) -> u32 {
@@ -34,9 +84,7 @@ mod test {
 
     #[test]
     fn example1() {
-        let p = Point::new(1, 1);
-        let s = "
-............
+        let s = "............
 ........0...
 .....0......
 .......0....
@@ -49,9 +97,5 @@ mod test {
 ............
 ............";
         assert_eq![part1(s), 14];
-    }
-
-    #[test]
-    fn example2() {
     }
 }
